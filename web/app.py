6b39e0cc2d906007970e7dfff03bdb25164055a5
akocukcu <akocukcu@gmail.com>
2020-04-25 18:12:57 +0300

    logging imported and config set

--- web/app.py
+++ web/app.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 api = Api(app)
